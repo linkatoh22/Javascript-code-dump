145671d17cc8d81b307c8eb2f22c7ce1bbf7b883
linkatoh22 <lintoh111@gmail.com>
2024-06-05 17:06:52 +0700

    commit

---
+++ basicDom/index.js
@@ -0,0 +1,5 @@
+console.log("Hi this is basic but yeah we have to try");
+
+
+document.getElementById("myH1").textContent="Hi";
+document.getElementById("myP").textContent="Hi this is basic but yeah we have to try";
