33f168bc78c8a6c3f067a807c8917194527d638a
linkatoh22 <lintoh111@gmail.com>
2024-06-05 17:25:16 +0700

    variables

---
+++ variables/index.js
@@ -0,0 +1,10 @@
+let age=16;
+let price = 10.99;
+let gpa = 2.8;
+document.getElementById("gpa").textContent=gpa;
+document.getElementById("price").textContent=price;
+document.getElementById("age").textContent=age;
+console.log(typeof gpa);
+console.log(`You are ${age} years old`);
+console.log(`You are ${gpa}`);
+console.log(`The price are ${price}`);
