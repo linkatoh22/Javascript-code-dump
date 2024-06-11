91e3f16d8db982d7493089fe570bd666b5ac747d
linkatoh22 <lintoh111@gmail.com>
2024-06-11 22:15:47 +0700

    strict equality

---
+++ Logical Operators/index.js
@@ -0,0 +1,20 @@
+const temp = 20;
+if (temp > 0 && temp <= 30 )
+    {
+        console.log("The weather is GOOD");
+    }
+else 
+    {
+        console.log("The weather is BAD")
+    }
+
+let isSunny = true;
+
+if (!isSunny)
+    {
+        console.log("kinda cloudy outside");
+    }
+    else
+    {
+        console.log("its sunny");
+    }
---
+++ strictEquality/index.js
@@ -0,0 +1,19 @@
+//STRICT EQUALITY
+// === COMPARE IF VALUES AND DATATYPE IS COMPATIBLE
+// !== COMPARE IF VALUES AND DATATYPE IS NOT COMPATIBLE
+const PI= 3.14;
+
+
+if(PI == "3.14") //NO STRICT
+    {
+        console.log("THIS IS PIE idk");
+    }
+
+if(PI ==="3.14")
+    {
+        console.log("HELL YEAH")
+    }
+else
+{
+    console.log("HECK NO");
+}
