56e45b6a06ea18fb6b61f575bffe04b286728f28
linkatoh22 <lintoh111@gmail.com>
2024-06-11 17:16:51 +0700

    Tenary Operation

---
+++ ifShortCut (Tenary Operator)/index.js
@@ -0,0 +1,23 @@
+/*
+let age=21;
+let message= age>=18? "You're an adult":"You are an minor";
+
+console.log(message);
+*/
+
+/*
+let time=16;
+
+let greeting =time<12?"Good Morning":"Good Afternoon";
+
+console.log(greeting);*/
+
+/*
+let isStudent= true;
+let message= isStudent?"You are a student":"You aren't a student";
+console.log(message);*/
+
+let purchaseAmount = 125;
+let discount = purchaseAmount>=100? 10:0;
+
+console.log(`Your disount Amount is ${discount} %`);
