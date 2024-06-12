9afd0db31fdac344dca57624e93f37f034938ccd
linkatoh22 <lintoh111@gmail.com>
2024-06-12 17:55:51 +0700

    function

---
+++ function/index.js
@@ -0,0 +1,28 @@
+
+function happyBirthday(username)
+{
+    console.log(`Happy birthday to ${username}\nHappy birthday, happy birthday and the start burn tonight`);
+}
+
+happyBirthday("Linh");
+
+function add (x,y)
+{
+    let result = x + y ;
+    return result;
+}
+let result= add(2 , 3);
+console.log(result);
+
+function substract (x , y) 
+{
+    let result = x - y;
+    return result;
+}
+
+function isEven (x )
+{
+    return x % 2 === 0 ? true : false;
+}
+
+console.log(isEven(9));
