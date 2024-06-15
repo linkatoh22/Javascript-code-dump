91dd551bf8ee7f50eed1677839b7c9d40979c7c0
linkatoh22 <lintoh111@gmail.com>
2024-06-15 23:56:39 +0700

    arrow function

---
+++ arrowFunction/index.js
@@ -0,0 +1,23 @@
+// arrow function = a concise way to write function expressions
+//                  good for simple functions that you use only once
+//                  (parameters) => some code
+
+//NORMAL
+function hello ()
+{
+    console.log("Hello");
+}
+// AROW FUNCTION
+const hello_arrow = (name,age) =>{ console.log(`Hello ${name}`) 
+    console.log(`You are ${age} `);
+};
+hello_arrow("Linh",20);
+
+
+//SECOND EXAMPLE
+
+setTimeout(hello_arrow("Linh",19),3000);
+
+//THIRD EXAMPLE
+
+const numbers = [1,2,3,4,5];
